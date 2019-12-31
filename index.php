54f4513c91eb2b8c2a6d23a6f37d4d6190152345
phidvss55 <phidv9855@gmail.com>
2019-12-31 12:20:19 +0700

    Codding navigation file

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
-<?php 
-    echo "Hello";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Shop</title>
 
-?>
+    <!-- Link customer css -->
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+    
+    <div class="main_wrapper">
+
+        <div class="header_wrapper">
+            <img src="./images/logo.gif" alt="Logo Image" style="float: left">
+            <img src="./images/ad_banner.gif" alt="Advertisment" style="float: right">
+        </div>
+
+        <!-- Navigation Bar Starts -->
+        <div id="navbar">
+            <ul id="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">All Products</a></li>
+                <li><a href="#">My Account</a></li>
+                <li><a href="#">Sign Up</a></li>
+                <li><a href="#">Shopping Cart</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <!--  -->
+        <div class="content_wrapper">
+            <div id="left_sidebar">Left Side</div>
+            <div id="right_content">Right Content</div>
+        </div>
+        <div class="footer">Footer Bar</div>
+    </div>
+
+</body>
+</html>
