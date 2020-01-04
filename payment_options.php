d01dc1cb15fc0e6c9916d3dc090f03c80c73d3dd
phidvss55 <phidv9855@gmail.com>
2020-01-04 13:03:34 +0700

    Adding method payment: offline, paypal

---
+++ payment_options.php
@@ -0,0 +1,5 @@
+<div style="padding:20px; text-align:center;">
+    <h2>Payment Options for you</h2>
+    <b>Pay With - </b><a href="http://www.paypal.com"><img src="./images/paypal.png" alt="Paypal options"></a><b> Or <a href="pay_offline.php">Pay Offline</a></b>
+    <b><p>If you selected "Pay Offline" option then please check your account to find Invoice. No for your order.</p></b>
+</div>
